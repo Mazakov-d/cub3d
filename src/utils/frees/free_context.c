6e26572363520d52c8dca171baa842e04c02071c
Mazakov-d <mazaridorian5@gmail.com>
2025-08-07 13:08:13 +0200

    repushing free_context

---
+++ src/utils/frees/free_context.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_context.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmazari <dmazari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 13:07:31 by dmazari           #+#    #+#             */
+/*   Updated: 2025/08/07 13:07:48 by dmazari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub.h"
+
+void free_context(t_context *ctx)
+{
+    if (!ctx)
+        return;
+
+    if (ctx->texture_data.walls[NO].img_name)
+        free(ctx->texture_data.walls[NO].img_name);
+    if (ctx->texture_data.walls[SO].img_name)
+        free(ctx->texture_data.walls[SO].img_name);
+    if (ctx->texture_data.walls[EA].img_name)
+        free(ctx->texture_data.walls[EA].img_name);
+    if (ctx->texture_data.walls[WE].img_name)
+        free(ctx->texture_data.walls[WE].img_name);
+    if (ctx->mlx)
+        free(ctx->mlx);
+    ft_free_tab((void **)ctx->map);
+}
